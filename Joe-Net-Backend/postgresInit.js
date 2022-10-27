b74cea0109b4a43e2586c86bb7ba5c6b6c3f7701
Joseph Sharkey <77642630+Joseph-Sharkey@users.noreply.github.com>
2022-10-27 21:50:49 +0100

    Update postgresInit.js

--- Joe-Net-Backend/postgresInit.js
+++ Joe-Net-Backend/postgresInit.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-	user: "joseph",
+	user: "",
 	host: "localhost",
-	database: "postgres",
-	password: "Hellopeople201590",
-	port: 5432
+	database: "",
+	password: "",
+	port: ""
 });
 
 
